9b7e302abb2a70f8c4b3e91acd8401cb6f4ef476
cenap35 <75336733+cenap35@users.noreply.github.com>
2026-05-15 01:03:17 +0300

    Setup React frontend architecture and auth services

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,20 @@
+import api from '../api/axios'
+
+export const login = async (email, password) => {
+  const response = await api.post('/auth/login', {
+    email,
+    password,
+  })
+
+  return response.data
+}
+
+export const register = async (fullName, email, password) => {
+  const response = await api.post('/auth/register', {
+    fullName,
+    email,
+    password,
+  })
+
+  return response.data
+}
