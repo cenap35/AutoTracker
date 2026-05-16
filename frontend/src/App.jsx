88bf8459e5c4b1e8311c1f2b190c8a9f9ef0afa1
cenap35 <75336733+cenap35@users.noreply.github.com>
2026-05-16 03:42:55 +0300

    Add protected frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,35 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import VehiclesPage from './pages/VehiclePage'
 import VehicleDetailPage from './pages/VehicleDetailPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-
-      <Route path="/login" element={<LoginPage />} />
-
-      <Route path="/register" element={<RegisterPage />} />
-
-      <Route path="/vehicles" element={<VehiclesPage />} />
-      
-      <Route path="/vehicles/:id" element={<VehicleDetailPage />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+    
+        <Route path="/login" element={<LoginPage />} />
+    
+        <Route path="/register" element={<RegisterPage />} />
+    
+        <Route
+          path="/vehicles"
+          element={
+            <ProtectedRoute>
+              <VehiclesPage />
+            </ProtectedRoute>
+          }
+        />
+    
+        <Route
+          path="/vehicles/:id"
+          element={
+            <ProtectedRoute>
+              <VehicleDetailPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
   )
 }
 
