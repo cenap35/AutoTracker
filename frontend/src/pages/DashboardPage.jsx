b70dcb589b22a1b1375dffacc573061cc6ce0b45
cenap35 <75336733+cenap35@users.noreply.github.com>
2026-05-16 16:44:06 +0300

    backend dashboard-front fetch

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
 import { getDashboardSummary } from "../services/dashboardService";
+import { getVehicles } from "../services/vehicleService";
+import { Link } from "react-router-dom";
 
 function DashboardPage() {
   const [summary, setSummary] = useState(null);
+  const [vehicles, setVehicles] = useState([]);
 
   useEffect(() => {
     const fetchSummary = async () => {
       try {
         const data = await getDashboardSummary();
         setSummary(data);
+        const vehiclesData = await getVehicles();
+        setVehicles(vehiclesData);
       } catch (err) {
         console.error(err);
       }
     };
-  
+
     fetchSummary();
   }, []);
 
@@ -23,18 +28,28 @@ function DashboardPage() {
 
   return (
     <div>
-    <h1>Dashboard</h1>
-  
-    <h2>Total Vehicles: {summary.totalVehicles}</h2>
-  
-    <h2>
-      Total Maintenance Records: {summary.totalMaintenanceRecords}
-    </h2>
-  
-    <h2>
-      Total Maintenance Cost: ₺{summary.totalMaintenanceCost}
-    </h2>
-  </div>
+      <h1>Dashboard</h1>
+
+      <h2>Total Vehicles: {summary.totalVehicles}</h2>
+
+      <h2>Total Maintenance Records: {summary.totalMaintenanceRecords}</h2>
+
+      <h2>Total Maintenance Cost: ₺{summary.totalMaintenanceCost}</h2>
+
+      <h2>My Vehicles</h2>
+
+      {vehicles.map((vehicle) => (
+        <div key={vehicle.id}>
+          <h3>
+            {vehicle.brand} {vehicle.model}
+          </h3>
+          <p>Year: {vehicle.year}</p>
+          <p>Plate: {vehicle.plateNumber}</p>
+          <p>Mileage: {vehicle.currentMileage}</p>
+          <Link to={`/vehicles/${vehicle.id}`}>Details</Link>
+        </div>
+      ))}
+    </div>
   );
 }
 
