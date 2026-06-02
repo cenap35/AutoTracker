eac7f4ac1cd388eee1240eaf50f92f981c193166
cenap35 <75336733+cenap35@users.noreply.github.com>
2026-06-02 05:14:06 +0300

    DOCKER Add Docker Compose and PostgreSQL container setup

--- backend/AutoTracker.Api/Program.cs
+++ backend/AutoTracker.Api/Program.cs
@@ -60,6 +60,15 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
+
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
